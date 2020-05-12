02f1f8d1e7c8e479a1b313e8cf5ce3d594c47e3f
SincerelyBrittany <BrittanyRussell1993@gmail.com>
2020-05-11 21:01:26 -0400

    changes responsiblity of sorting the price from aruby task to a sql task

--- lib/room.rb
+++ lib/room.rb
@@ -27,32 +27,61 @@ class Room
 
 
 
+  # def self.all
+  #   sql = <<-SQL
+  #     SELECT * FROM rooms;
+  #   SQL
+  #
+  #   rows = DB[:connection].execute(sql)
+  #
+  #   #go from a row [1, title, date, price, url] to an instance #<room>
+  #
+  #   rows.collect do |row|
+  #     self.new_from_db(row)
+  #   end
+  #
+  # end
+
+
+  # def self.by_price(order = "ASC") #WHY HAVE RUBY DO THIS WHEN SQL CAN SORT
+  #   case order
+  #   when "ASC"
+  #     self.all.sort_by{|r| r.price}
+  #   when "DESC"
+  #     self.all.sort_by{|r| r.price}.reverse
+  #   end
+  # #Room.by_price("ASC") #=> lowest price room first
+  # #Room.by Price("DESC") #+> highest price room first
+  # end
+
+
+  def self.new_from_rows(rows)
+    rows.collect do |row|
+      self.new_from_db(row)
+    end
+  end
+
   def self.all
     sql = <<-SQL
       SELECT * FROM rooms;
     SQL
 
     rows = DB[:connection].execute(sql)
-
-    #go from a row [1, title, date, price, url] to an instance #<room>
-
-    rows.collect do |row|
-      self.new_from_db(row)
-    end
+    self.new_from_rows(rows)
 
   end
 
 
-  def self.by_price(order = "ASC")
-    case order
-    when "ASC"
-      self.all.sort_by{|r| r.price}
-    when "DESC"
-      self.all.sort_by{|r| r.price}.reverse
+  def self.by_price(order="ASC")
+    sql = <<-SQL
+      SELECT * FROM rooms ORDER BY price #{order};
+    SQL
+    rows = DB[:connection].execute(sql)
+    self.new_from_rows(rows)
     end
-  #Room.by_price("ASC") #=> lowest price room first
-  #Room.by Price("DESC") #+> highest price room first
-  end
+
+
+
 
   def save
     #I need a database!!!
